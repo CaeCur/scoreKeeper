3cb9ee1913f0fe5e034638b4171c06b6802df7d8
Caelan Curry <caelan.curry@gmail.com>
2021-01-13 00:15:22 +0200

    Added game over logic for hardcoded winning score. Scores stop counting once either player reaches 5 points.

--- app.js
+++ app.js
@@ -4,19 +4,35 @@ const p1IncreaseButton = document.querySelector("#p1Increase");
 const p2IncreaseButton = document.querySelector("#p2Increase");
 const resetButton = document.querySelector("#reset");
 
+let winningScore = 5;
+let isGameOver = false;
+
 p1IncreaseButton.addEventListener("click", function () {
-	const currentScore = parseInt(p1Score.innerText);
-	const newScore = currentScore + 1;
-	p1Score.innerText = newScore;
+	const currentScore = parseInt(p1Score.textContent);
+
+	if (!isGameOver) {
+		let newScore = currentScore + 1;
+		if (newScore === winningScore) {
+			isGameOver = true;
+		}
+		p1Score.textContent = newScore;
+	}
 });
 
 p2IncreaseButton.addEventListener("click", function () {
-	const currentScore = parseInt(p2Score.innerText);
-	const newScore = currentScore + 1;
-	p2Score.innerText = newScore;
+	const currentScore = parseInt(p2Score.textContent);
+
+	if (!isGameOver) {
+		let newScore = currentScore + 1;
+		if (newScore === winningScore) {
+			isGameOver = true;
+		}
+		p2Score.textContent = newScore;
+	}
 });
 
 resetButton.addEventListener("click", function () {
-	p1Score.innerText = 0;
-	p2Score.innerText = 0;
+	p1Score.textContent = 0;
+	p2Score.textContent = 0;
+	isGameOver = false;
 });
