7a404690d957cf2533cae37449268f35cffa36f3
Caelan Curry <caelan.curry@gmail.com>
2021-01-19 20:20:08 +0200

    Refactoring

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
-const p1Score = document.querySelector("#p1Score");
-const p2Score = document.querySelector("#p2Score");
-const p1IncreaseButton = document.querySelector("#p1Increase");
-const p2IncreaseButton = document.querySelector("#p2Increase");
+const p1 = {
+	score  : document.querySelector("#p1Score"),
+	button : document.querySelector("#p1Increase")
+};
+const p2 = {
+	score  : document.querySelector("#p2Score"),
+	button : document.querySelector("#p2Increase")
+};
+// const p1Score = document.querySelector("#p1Score");
+// const p2Score = document.querySelector("#p2Score");
+// const p1IncreaseButton = document.querySelector("#p1Increase");
+// const p2IncreaseButton = document.querySelector("#p2Increase");
 const resetButton = document.querySelector("#reset");
 const winningScoreSelect = document.querySelector("#roundsSelect");
 
@@ -10,13 +18,13 @@ let isGameOver = false;
 
 // Functions //
 function resetGame () {
-	p1Score.textContent = 0;
-	p1Score.classList.remove("has-text-success", "has-text-danger");
-	p2Score.textContent = 0;
-	p2Score.classList.remove("has-text-success", "has-text-danger");
+	p1.score.textContent = 0;
+	p1.score.classList.remove("has-text-success", "has-text-danger");
+	p2.score.textContent = 0;
+	p2.score.classList.remove("has-text-success", "has-text-danger");
 	isGameOver = false;
-	p1IncreaseButton.disabled = false;
-	p2IncreaseButton.disabled = false;
+	p1.button.disabled = false;
+	p2.button.disabled = false;
 }
 
 function incrementScore (playerNumber, opponentNumber) {
@@ -28,8 +36,8 @@ function incrementScore (playerNumber, opponentNumber) {
 			isGameOver = true;
 			playerNumber.classList.add("has-text-success");
 			opponentNumber.classList.add("has-text-danger");
-			p1IncreaseButton.disabled = true;
-			p2IncreaseButton.disabled = true;
+			p1.button.disabled = true;
+			p2.button.disabled = true;
 		}
 		playerNumber.textContent = newScore;
 	}
@@ -37,12 +45,12 @@ function incrementScore (playerNumber, opponentNumber) {
 // Functions End //
 
 // Score Increments //
-p1IncreaseButton.addEventListener("click", function () {
-	incrementScore(p1Score, p2Score);
+p1.button.addEventListener("click", function () {
+	incrementScore(p1.score, p2.score);
 });
 
-p2IncreaseButton.addEventListener("click", function () {
-	incrementScore(p2Score, p1Score);
+p2.button.addEventListener("click", function () {
+	incrementScore(p2.score, p1.score);
 });
 // Score Increments End //
 
